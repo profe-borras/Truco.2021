d3fb2a5f7959ef7e6dc81f8d542cd944421a6deb
SamMontivero <samuel.montivero@mi.unc.edu.ar>
2021-10-01 21:37:01 -0300

    Revert "Eliminar codigo comentado"
    This reverts commit 3cc0c3e080f81e9b9e5e8f548b2e4f13245164bb.

--- ManuelBelgrano.Truco/Program.cs
+++ ManuelBelgrano.Truco/Program.cs
@@ -32,6 +32,11 @@ namespace ManuelBelgrano.Truco
              
 
             int[] numeros = new int[] { 3, 0, 77, 9 };
+            //intercambiar el elemento de la posición 2 con el elmento de la posición 0
+            //int num = numeros[2];
+            //numeros[2] = numeros[0];
+            //numeros[0] = num; //By Juampi
+
             //algoritmo de Fisher-Yates
             Random rnd = new Random();
             for (int i = numeros.Length - 1; i > 0; i--) {
@@ -58,7 +63,82 @@ namespace ManuelBelgrano.Truco
                 }
             }
 
+            /*
+             int i = numeros.Lenght
+while (i > 1) {
+ 
+        int j = rng.Next(i);  
+        int swap = numeros[j];  
+        numeros[j] = numeros[i];  
+        numeros[i] = swap;
+        i--;
+
+             
+             */
+
             Console.ReadKey();
+
+            //var rndm = new Random();
+
+            //var listadoDeIndicesDelMazo = new List<int>();
+
+            ////Objetivo: obtener 6 números distintos del 0 al 39 ionclusive
+
+            //do
+            //{
+            //    //Entrada
+            //    var idx = rndm.Next(0, 40);
+            //    //proceso
+            //    if (listadoDeIndicesDelMazo.Contains(idx) == true) continue;
+            //    listadoDeIndicesDelMazo.Add(idx);
+
+            //} while (listadoDeIndicesDelMazo.Count < 6); //salida
+
+
+
+            //var cartasJugador1 = new List<CartaDeTruco>();
+            //for (var i = 0; i < 3; i++)
+            //{
+
+            //    var indiceDelMazo = listadoDeIndicesDelMazo[i];
+            //    var carta = mazo[indiceDelMazo];
+            //    cartasJugador1.Add(carta);
+            //}
+
+            //var cartasJugador2 = new List<CartaDeTruco>();
+            //for (var i = 3; i < 6; i++)
+            //{
+            //    var indiceDelMazo = listadoDeIndicesDelMazo[i];
+            //    var carta = mazo[indiceDelMazo];
+            //    cartasJugador2.Add(carta);
+            //}
+
+            //Console.WriteLine("Cartas del jugador 1");
+            //foreach (var item in cartasJugador1.OrderByDescending(c=>c.Numero))
+            //{
+            //    Console.WriteLine($"{item.Numero} de {item.Palo}");
+            //}
+
+            //Console.WriteLine("Cartas del jugador 2");
+            //foreach (var item in cartasJugador2.OrderBy(c => c.Numero))
+            //{
+            //    Console.WriteLine($"{item.Numero} de {item.Palo}");
+            //}
+
+            /*
+              var numerosGenerados = new List<int>();
+            var rnd = new Random();
+            while (numerosGenerados.Count <= 30)
+            {
+                var valorRandom = rnd.Next(0, 30);
+                if (numerosGenerados.Contains(valorRandom)) continue;
+
+                numerosGenerados.Add(valorRandom);
+                Console.WriteLine(valorRandom);
+            }
+
+            Console.ReadLine();
+            */
 
 
 
